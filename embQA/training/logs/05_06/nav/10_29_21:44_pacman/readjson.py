f2a73360c4bdf194e389e8b9fcb1a46a5327979e
hyadav <himanshimj@github.com>
2018-10-31 00:13:54 +0000

    Adding Logs

---
+++ embQA/training/logs/05_06/nav/10_29_21:44_pacman/readjson.py
@@ -0,0 +1,4 @@
+import json
+json_file = "10_24_04:54_pacman.json"
+with open('data.txt') as json_file:
+	data = json.load(json_file)
---
+++ embQA/training/logs/05_06/nav/pyplotting.py
@@ -0,0 +1,28 @@
+import json
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+iters = []
+stat = []
+
+config = json.loads(open('10_30_04:12_pacman/train_1.json').read())
+
+
+statistics = config['stats']
+
+
+plt.title('Training Progress in terms of Planner Loss')
+plt.xlabel('Iterations')
+plt.ylabel('Mean Loss')	
+		
+
+
+for i in range(len(statistics)):
+ iters.append(i)
+ stat.append(statistics[i][0])
+ plt.plot(iters,stat)
+
+
+plt.savefig('myfig')
+
