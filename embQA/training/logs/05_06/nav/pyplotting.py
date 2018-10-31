e2d62895e585c2acc184c76c4a890edfcdfab389
hyadav <himanshimj@github.com>
2018-10-31 00:25:10 +0000

    New Plots Added

--- embQA/training/logs/05_06/nav/pyplotting.py
+++ embQA/training/logs/05_06/nav/pyplotting.py
@@ -12,19 +12,38 @@ config = json.loads(open('10_30_04:12_pacman/train_1.json').read())
 statistics = config['stats']
 
 
-plt.title('Training Progress in terms of Planner Loss')
+plt.title('Training Progress (Navigation) in terms of Planner Loss')
 plt.xlabel('Iterations')
 plt.ylabel('Mean Loss')	
 		
 
 
 for i in range(len(statistics)):
- i
+ print(i)
  iters.append(i)
- statistics[i][0]
+ print(statistics[i][0])
  stat.append(statistics[i][0])
  
 
 plt.plot(iters,stat)
-plt.savefig('myfig')
+plt.savefig('planner_losses')
+
+
+iters1 = []
+stat1 = []
+plt.title('Training Progress (Navigation) in terms of Controller Loss')
+plt.xlabel('Iterations')
+plt.ylabel('Mean Loss')	
+		
+
+
+for i in range(len(statistics)):
+ print(i)
+ iters1.append(i)
+ print(statistics[i][1])
+ stat1.append(statistics[i][1])
+ 
+
+plt.plot(iters1,stat1)
+plt.savefig('controller_losses')
 
